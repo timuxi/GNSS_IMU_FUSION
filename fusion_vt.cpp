9d6d9467bff95b43cba04246490b55d40ba4bbb2
timuxi <xuchengyang@echaincity.com>
2022-08-22 16:43:18 +0800

    update

--- fusion_vt.cpp
+++ fusion_vt.cpp
@@ -16,7 +16,6 @@
 
 using namespace std;
 
-const double epsilon = 1e-12; //小于该数判断为0
 static int ret;
 static int fd_imu;
 static int fd_gps;
@@ -41,280 +40,11 @@ double a[3], w[3], Angle[3];
 // gps数据
 vector<string> gpsData(3);
 
-template <class Type>
-Type stringToNum(const string &str)
+
+int uart_open(int fd, const char *pathname)
 {
-  istringstream iss(str);
-  Type num;
-  iss >> num;
-  return num;
-}
-
-vector<vector<double>> creatmatrix(int h, int l)
-{
-  vector<vector<double>> v;
-  for (int i = 0; i < h; i++)
-  {
-    vector<double> v1(l, 0);
-    v.push_back(v1);
-  }
-  return v;
-}
-
-//矩阵相加
-vector<vector<double>> plusMartrix(vector<vector<double>> A, vector<vector<double>> B)
-{
-  int h = A.size();
-  int l = A[0].size();
-  vector<vector<double>> C;
-  C = creatmatrix(h, l);
-
-  for (int i = 0; i < h; i++)
-  {
-    for (int j = 0; j < l; j++)
-    {
-      C[i][j] = A[i][j] + B[i][j];
-      if (abs(C[i][j]) < epsilon)
-      {
-        C[i][j] = 0.0;
-      }
-    }
-  }
-  return C;
-}
-
-//矩阵相乘
-vector<vector<double>> multiply(vector<vector<double>> A, vector<vector<double>> B)
-{
-  int A_h = A.size();
-  int A_l = A[0].size();
-  int B_h = B.size();
-  int B_l = B[0].size();
-  if (A_l != B_h)
-  {
-    cout << "两矩阵维数无法相乘" << endl;
-    exit(0);
-  }
-  vector<vector<double>> C = creatmatrix(A_h, B_l);
-  for (int i = 0; i < A_h; i++)
-  {
-    for (int j = 0; j < B_l; j++)
-    {
-      C[i][j] = 0;
-      for (int k = 0; k < A_l; k++)
-      {
-        C[i][j] += A[i][k] * B[k][j];
-      }
-      if (abs(C[i][j]) < epsilon)
-      {
-        C[i][j] = 0.0;
-      }
-      // cout<<C[i][j]<<"\t";
-    }
-    // cout<<endl;
-  }
-  return C;
-}
-
-//矩阵与数字相乘
-vector<vector<double>> multiplyWithNumber(vector<vector<double>> A, double num)
-{
-  int h = A.size();
-  int l = A[0].size();
-  vector<vector<double>> C;
-  C = creatmatrix(h, l);
-
-  for (int i = 0; i < A.size(); i++)
-  {
-    for (int j = 0; j < A[i].size(); j++)
-    {
-      C[i][j] = A[i][j] * num;
-    }
-  }
-  return C;
-}
-
-//矩阵相减
-vector<vector<double>> minusMatrix(vector<vector<double>> A, vector<vector<double>> B)
-{
-  int h = A.size();
-  int l = A[0].size();
-  vector<vector<double>> C;
-  C = creatmatrix(h, l);
-
-  for (int i = 0; i < h; i++)
-  {
-    for (int j = 0; j < l; j++)
-    {
-      C[i][j] = A[i][j] - B[i][j];
-      if (abs(C[i][j]) < epsilon)
-      {
-        C[i][j] = 0.0;
-      }
-    }
-  }
-  return C;
-}
-
-//转置矩阵AT
-vector<vector<double>> trans(vector<vector<double>> A)
-{
-  vector<vector<double>> AT = creatmatrix(A[0].size(), A.size());
-  int h = AT.size();
-  int l = AT[0].size();
-  for (int i = 0; i < h; i++)
-  {
-    for (int j = 0; j < l; j++)
-    {
-      AT[i][j] = A[j][i];
-    }
-  }
-  return AT;
-}
-
-//逆矩阵inv_A
-vector<vector<double>> inverse(vector<vector<double>> A)
-{
-  if (A.size() != A[0].size())
-  {
-    cout << "输入矩阵维数不合法" << endl;
-    exit(0);
-  }
-  int n = A.size();
-  vector<vector<double>> inv_A = creatmatrix(n, n);
-  vector<vector<double>> L = creatmatrix(n, n);
-  vector<vector<double>> U = creatmatrix(n, n);
-  vector<vector<double>> inv_L = creatmatrix(n, n);
-  vector<vector<double>> inv_U = creatmatrix(n, n);
-  // L矩阵对角元素为1
-  for (int i = 0; i < n; i++)
-  {
-    L[i][i] = 1;
-  }
-  // U矩阵第一行
-  for (int i = 0; i < n; i++)
-  {
-    U[0][i] = A[0][i];
-  }
-  // L矩阵第一列
-  for (int i = 1; i < n; i++)
-  {
-    L[i][0] = 1.0 * A[i][0] / A[0][0];
-  }
-
-  //计算LU上下三角
-  for (int i = 1; i < n; i++)
-  {
-    //计算U（i行j列）
-    for (int j = i; j < n; j++)
-    {
-      double tem = 0;
-      for (int k = 0; k < i; k++)
-      {
-        tem += L[i][k] * U[k][j];
-      }
-      U[i][j] = A[i][j] - tem;
-      if (abs(U[i][j]) < epsilon)
-      {
-        U[i][j] = 0.0;
-      }
-    }
-    //计算L（j行i列）
-    for (int j = i; j < n; j++)
-    {
-      double tem = 0;
-      for (int k = 0; k < i; k++)
-      {
-        tem += L[j][k] * U[k][i];
-      }
-      L[j][i] = 1.0 * (A[j][i] - tem) / U[i][i];
-      if (abs(L[i][j]) < epsilon)
-      {
-        L[i][j] = 0.0;
-      }
-    }
-  }
-  // L U剩余位置设为0
-  for (int i = 0; i < n; i++)
-  {
-    for (int j = 0; j < n; j++)
-    {
-      if (i > j)
-      {
-        U[i][j] = 0.0;
-      }
-      else if (i < j)
-      {
-        L[i][j] = 0.0;
-      }
-    }
-  }
-  // LU求逆
-  //求矩阵U的逆
-  for (int i = 0; i < n; i++)
-  {
-    inv_U[i][i] = 1 / U[i][i]; // U对角元素的值，直接取倒数
-    for (int k = i - 1; k >= 0; k--)
-    {
-      double s = 0;
-      for (int j = k + 1; j <= i; j++)
-      {
-        s = s + U[k][j] * inv_U[j][i];
-      }
-      inv_U[k][i] = -s / U[k][k]; //迭代计算，按列倒序依次得到每一个值，
-      if (abs(inv_U[k][i]) < epsilon)
-      {
-        inv_U[k][i] = 0.0;
-      }
-    }
-  }
-  //求矩阵L的逆
-  for (int i = 0; i < n; i++)
-  {
-    inv_L[i][i] = 1; // L对角元素的值，直接取倒数，这里为1
-    for (int k = i + 1; k < n; k++)
-    {
-      for (int j = i; j <= k - 1; j++)
-      {
-        inv_L[k][i] = inv_L[k][i] - L[k][j] * inv_L[j][i];
-        if (abs(inv_L[k][i]) < epsilon)
-        {
-          inv_L[k][i] = 0.0;
-        }
-      }
-    }
-  }
-  inv_A = multiply(inv_U, inv_L);
-  return inv_A;
-}
-
-//显示矩阵
-void show_matrix(vector<vector<double>> A)
-{
-  int h = A.size();
-  int l = A[0].size();
-  cout << "---------" << endl;
-  for (int i = 0; i < h; i++)
-  {
-    for (int j = 0; j < l; j++)
-    {
-      cout << A[i][j] << "\t";
-    }
-    cout << endl;
-  }
-  cout << "---------" << endl;
-}
-
-//单个数值的矩阵取出数值
-double getnum(vector<vector<double>> A)
-{
-  return A[0][0];
-}
-
-int uart_open(int fd_imu, const char *pathname)
-{
-  fd_imu = open(pathname, O_RDWR | O_NOCTTY);
-  if (-1 == fd_imu)
+  fd = open(pathname, O_RDWR | O_NOCTTY);
+  if (-1 == fd)
   {
     perror("Can't Open Serial Port");
     return (-1);
@@ -328,7 +58,7 @@ int uart_open(int fd_imu, const char *pathname)
     printf("standard input is not a terminal device\n");
   else
     printf("isatty success!\n");
-  return fd_imu;
+  return fd;
 }
 
 //串口名、波特率、数据位、奇偶校验位、停止位
@@ -589,75 +319,12 @@ void getZ(char *chr)
   gpsData[2] = gps3;
 }
 
-/**
- * t  采样频率
- * K  卡尔曼增益
- * N  先验估计
- * NN 后验估计
- * Q  系统过程噪声协方差矩阵
- * R  测量噪声
- * ka imu中获取的加速度
- * z  gnss中获取的高程
- */
 
-double ka = 0;
-double z = 0;
-double t = 1;
-double R = 0.003112 * t;
-vector<vector<double>> N = creatmatrix(2, 1);
-vector<vector<double>> F = creatmatrix(2, 2);
-vector<vector<double>> B = creatmatrix(2, 1);
-vector<vector<double>> P = creatmatrix(2, 2);
-vector<vector<double>> Q = creatmatrix(2, 2);
-vector<vector<double>> H = creatmatrix(1, 2);
-vector<vector<double>> G = creatmatrix(2, 2);
-vector<vector<double>> K = creatmatrix(2, 1);
-vector<vector<double>> NN = creatmatrix(2, 1);
-
-void kalmanInit()
-{
-  F[0][0] = 1;
-  F[0][1] = t;
-  F[1][0] = 0;
-  F[1][1] = 1;
-
-  B[0][0] = 1 / 2 * t * t;
-  B[1][0] = t;
-
-  P[0][0] = 1;
-  P[0][1] = 0;
-  P[1][0] = 0;
-  P[1][1] = 1;
-
-  Q[0][0] = 0.001;
-  Q[0][1] = 0;
-  Q[1][0] = 0;
-  Q[1][1] = 0.01;
-
-  H[0][0] = 1;
-  H[0][1] = 0;
-
-  G[0][0] = 1;
-  G[1][1] = 1;
-}
-
-void kalmanFilter(double _ka, string _z)
-{
-  ka = _ka;
-  z = stringToNum<double>(_z);
-  // Predict
-  N = plusMartrix(multiply(F, N), multiplyWithNumber(B, ka));
-  P = plusMartrix(multiply(multiply(F, P), trans(F)), Q);
-  // Updata
-  K = multiplyWithNumber(multiply(P, trans(H)), 1.0 / (getnum(multiply(multiply(H, P), trans(H))) + R));
-  NN = plusMartrix(N, multiplyWithNumber(K, z - getnum(multiply(H, N))));
-  P = multiply(minusMatrix(G, multiply(K, H)), P);
-  N = NN;
-}
 
 void usbInit()
 {
   // USB口号/dev/ttyUSBn
+  // imu
   fd_imu = uart_open(fd_imu, "/dev/ttyUSBimu");
   if (fd_imu == -1)
   {
@@ -665,6 +332,7 @@ void usbInit()
     exit(EXIT_FAILURE);
   }
 
+  // gps
   fd_gps = uart_open(fd_gps, "/dev/ttyUSBgps");
   if (fd_gps == -1)
   {
@@ -696,8 +364,6 @@ string getTime()
   return ss.str();
 }
 
-float pos_z;
-
 void printGpsDataAll(char *chr)
 {
   for (int i = 0; i < gps_data_size; i++)
@@ -718,8 +384,8 @@ int main(void)
 
   int RW_flag = 0;
   int cnt = 0;
-
   ofstream OutFile(getTime());
+  
   while (1)
   {
     if (RW_flag == 0)
--- test.cpp
+++ test.cpp
@@ -1,75 +1,7 @@
 #include <iostream>
 #include<vector>
-
 using namespace std;
-const double epsilon=1e-12;  //小于该数判断为0
-//创建 h行l列的矩阵，并将初始各值设定为0
-vector<vector<double>> creatmatrix(int h,int l)
-{
-	vector<vector<double>> v;
-	for (int i = 0; i < h; i++)
-	{
-		vector<double>v1(l,0);
-		v.push_back(v1);
-	}
-	return v;
-}
-//矩阵A*矩阵B=矩阵C，并返回
-vector<vector<double>> multiply(const vector<vector<double>>&A,const vector<vector<double>>&B)
-{
-	int A_h=A.size();
-	int A_l=A[0].size();
-	int B_h=B.size();
-	int B_l=B[0].size();
-	if(A_l !=B_h)
-	{
-		cout<<"两矩阵维数无法相乘"<<endl;
-		exit(0);
-	}
-	vector<vector<double>> C=creatmatrix(A_h,B_l);
-	for (int i = 0; i < A_h; i++)
-	{
-		for (int j = 0; j < B_l; j++)
-		{
-			C[i][j]=0;
-			for (int k = 0; k < A_l; k++)
-			{
-				C[i][j] +=A[i][k]*B[k][j];
-			}
-			if (abs(C[i][j])<epsilon)
-			{
-				C[i][j]=0.0;
-			}
-			//cout<<C[i][j]<<"\t";
-		}
-		//cout<<endl;
-	}
-	return C;
-}
-
-void show_matrix(const vector<vector<double>> &A)
-{
-	int h=A.size();
-	int l=A[0].size();
-	for (int i = 0; i < h; i++)
-	{
-		for (int j = 0; j < l; j++)
-		{
-			cout<<A[i][j]<<"\t";
-		}
-		cout<<endl;
-	}
-}
-
 
 int main(){
-    vector<vector<double>> H = creatmatrix(1,2);
-    H[0][0] = 1;
-    H[0][1] = 2;
-    vector<vector<double>> B = creatmatrix(2,1);
-    B[0][0] = 1;
-    B[1][0] = 2;
-    vector<vector<double>> temp = multiply(H,B);
-	double a = temp[0][0];
-	printf("%f",a);
+	printf("123");
 }
